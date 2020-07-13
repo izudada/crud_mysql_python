e9c21df2711d6e88bf2f786ef10686f500184a58
Udeagbala Anthony <tonyudeagbala@gmail.com>
2020-07-13 11:54:10 +0000

    Added get_log and get_logs functions to main.py

--- main.py
+++ main.py
@@ -9,6 +9,31 @@ def add_log(text, user):
 
     print("Added log {}".format(log_id))
 
-add_log("This is a new user", "Chukwu Ifeanyi")
-add_log("This is a new user being added", "Adimoha SAmuel")
-add_log("Another user is being added", "Ehumadu REginald")
+
+def get_logs():
+    sql = ("SELECT * FROM logs ORDER BY created DESC")
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    
+    for row in result:
+        print(row[2])
+
+
+def get_log(digit):
+    sql = ("SELECT * FROM logs WHERE id = %s")
+
+    cursor.execute(sql,(digit,))
+    result = cursor.fetchone()
+    
+    for row in result:
+        print(row)
+
+
+# add_log("This is a new user", "Chukwu Ifeanyi")
+# add_log("This is a new user being added", "Adimoha SAmuel")
+# add_log("Another user is being added", "Ehumadu REginald")
+
+#get_logs()
+
+get_log(2)
